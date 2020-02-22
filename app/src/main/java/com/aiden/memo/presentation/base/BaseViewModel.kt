7fc8a1ea96ff99a5ead955f2f82e500cff7d78ed
Aiden <sunminlee89@gmail.com>
2020-02-22 18:35:31 +0900

    configuration change 때 데이터 두번 발행안되게 Event 객체 처리 

--- app/src/main/java/com/aiden/memo/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/aiden/memo/presentation/base/BaseViewModel.kt
@@ -3,13 +3,14 @@ package com.aiden.memo.presentation.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.aiden.memo.presentation.event.Event
 
 abstract class BaseViewModel : ViewModel() {
 
-    private val _isDataLoading = MutableLiveData(true)
+    protected val _isDataLoading = MutableLiveData(true)
     val isDataLoading: LiveData<Boolean> get() = _isDataLoading
 
-    private val _isDataLoadingError = MutableLiveData(false)
-    val isDataLoadingError: LiveData<Boolean> get() = _isDataLoadingError
+    protected val _isDataLoadingError = MutableLiveData(Event("" to false))
+    val isDataLoadingError: LiveData<Event<Pair<String, Boolean>>> get() = _isDataLoadingError
 
 }
