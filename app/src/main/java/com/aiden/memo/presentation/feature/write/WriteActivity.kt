18cfa3fca43cc137a28d8c3d3c8dc214ef69c50e
Aiden <sunminlee89@gmail.com>
2020-02-24 03:43:31 +0900

    링크 추가 기능 추가

--- app/src/main/java/com/aiden/memo/presentation/feature/write/WriteActivity.kt
+++ app/src/main/java/com/aiden/memo/presentation/feature/write/WriteActivity.kt
@@ -1,7 +1,11 @@
 package com.aiden.memo.presentation.feature.write
 
 import android.Manifest
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
 import com.aiden.memo.R
 import com.aiden.memo.databinding.ActivityWriteBinding
 import com.aiden.memo.presentation.base.BaseActivity
@@ -9,24 +13,28 @@ import com.aiden.memo.presentation.event.EventObserver
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import gun0912.tedbottompicker.TedBottomPicker
+import kotlinx.android.synthetic.main.dialog_default.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class WriteActivity : BaseActivity<ActivityWriteBinding, WriteViewModel>(R.layout.activity_write) {
     override val viewModel by viewModel<WriteViewModel>()
     private val intentKeyWriteType = "writeType"
+    private val intentKeyId = "id"
     private val imageMaxCount = 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val writeType = intent?.getSerializableExtra(intentKeyWriteType) as WriteType
-        setLayout(writeType)
-        setViewModel(writeType)
+        val memoId = intent?.getStringExtra(intentKeyId)
+        setLayout()
+        setViewModel(writeType, memoId)
     }
 
-    private fun setLayout(writeType: WriteType) {
+    private fun setLayout() {
         binding {
-            writeRvSeletedImage.adapter = SelectedImageListAdapter()
+            writeRvSeletedImage.adapter = SelectedImageListAdapter(viewModel)
+            writeRvImageLink.adapter = LinkListAdapter(viewModel)
 
             writeBtnImageSelect.setOnClickListener {
                 val permissionListener = object : PermissionListener {
@@ -37,6 +45,11 @@ class WriteActivity : BaseActivity<ActivityWriteBinding, WriteViewModel>(R.layou
                             .setCompleteButtonText(getString(R.string.write_select_image_complete))
                             .setEmptySelectionText(getString(R.string.write_select_image_no_select))
                             .setSelectMaxCount(imageMaxCount)
+                            .setSelectedUriList(viewModel.imageUriList.value?.map {
+                                Uri.parse(
+                                    it
+                                )
+                            })
                             .showMultiImage {
                                 viewModel.setImageList(it)
                             }
@@ -59,6 +72,14 @@ class WriteActivity : BaseActivity<ActivityWriteBinding, WriteViewModel>(R.layou
                     .check()
             }
 
+            writeBtnLinkSelect.setOnClickListener {
+                showDefaultDialog(onYes = {
+                    if (it.isNotEmpty()) {
+                        viewModel.addImageLink(it)
+                    }
+                })
+            }
+
             writeBtnSave.setOnClickListener {
                 viewModel.checkData().run {
                     if (this) viewModel.saveMemo()
@@ -68,15 +89,19 @@ class WriteActivity : BaseActivity<ActivityWriteBinding, WriteViewModel>(R.layou
         }
     }
 
-    private fun setViewModel(writeType: WriteType) {
-        viewModel.setWriteType(writeType)
+    private fun setViewModel(
+        writeType: WriteType,
+        memoId: String?
+    ) {
+        viewModel.setWriteType(writeType, memoId)
+
         viewModel.isDataLoadingError.observe(this, EventObserver {
             if (it.second) {
                 toastM("${it.first}")
             }
         })
 
-        viewModel.imageUriList.observe(this, EventObserver {
+        viewModel.imageUriList.observe(this, Observer {
             (binding.writeRvSeletedImage.adapter as SelectedImageListAdapter).setList(it)
         })
 
@@ -91,5 +116,33 @@ class WriteActivity : BaseActivity<ActivityWriteBinding, WriteViewModel>(R.layou
                 }
             }
         })
+
+        viewModel.memo.observe(this, EventObserver {
+            viewModel.setMemoData()
+        })
+
+        viewModel.imageLinkList.observe(this, Observer {
+            (binding.writeRvImageLink.adapter as LinkListAdapter).setList(it)
+        })
+    }
+
+    private fun showDefaultDialog(
+        onYes: (String) -> Unit
+    ) {
+        val builder = AlertDialog.Builder(this)
+        val view = layoutInflater.inflate(
+            R.layout.dialog_default,
+            null
+        )
+        builder.setView(view).setCancelable(true)
+        val dialog = builder.create()
+        dialog.show()
+        view.dialog_default_no.setOnClickListener {
+            dialog.dismiss()
+        }
+        view.dialog_default_yes.setOnClickListener {
+            dialog.dismiss()
+            onYes(view.dialog_default_et.text.toString())
+        }
     }
 }
--- app/src/main/java/com/aiden/memo/presentation/feature/write/WriteViewModel.kt
+++ app/src/main/java/com/aiden/memo/presentation/feature/write/WriteViewModel.kt
@@ -3,6 +3,7 @@ package com.aiden.memo.presentation.feature.write
 import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.viewModelScope
 import com.aiden.memo.domain.entity.Memo
 import com.aiden.memo.domain.entity.ThumbnailType
@@ -11,9 +12,11 @@ import com.aiden.memo.domain.usecase.InsertMemoUseCase
 import com.aiden.memo.domain.usecase.UpdateMemoUseCase
 import com.aiden.memo.presentation.base.BaseViewModel
 import com.aiden.memo.presentation.event.Event
+import com.aiden.memo.presentation.model.MemoModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.IOException
+import java.util.*
 
 class WriteViewModel(
     private val getMemoUseCase: GetMemoUseCase,
@@ -24,12 +27,11 @@ class WriteViewModel(
     val title = MutableLiveData<String>()
     val body = MutableLiveData<String>()
 
-    private val _imageUriList = MutableLiveData<Event<List<String>>>()
-    val imageUriList: LiveData<Event<List<String>>> get() = _imageUriList
+    private val _imageUriList = MutableLiveData<List<String>>()
+    val imageUriList: LiveData<List<String>> get() = _imageUriList
 
-    private val _imageLinkList = MutableLiveData<Event<List<String>>>()
-    val imageLinkList: LiveData<Event<List<String>>> get() = _imageLinkList
-
+    private val _imageLinkList = MutableLiveData<List<String>>()
+    val imageLinkList: LiveData<List<String>> get() = _imageLinkList
 
     private val _writeType = MutableLiveData<WriteType>()
     val writeType: LiveData<WriteType> get() = _writeType
@@ -37,17 +39,71 @@ class WriteViewModel(
     private val _status = MutableLiveData<Event<WriteStatusType>>()
     val status: LiveData<Event<WriteStatusType>> get() = _status
 
-    fun setWriteType(writeType: WriteType) = run {
+    private var _memo: LiveData<Event<MemoModel>> = MutableLiveData()
+    val memo: LiveData<Event<MemoModel>> get() = _memo
+
+    fun setWriteType(
+        writeType: WriteType,
+        memoId: String?
+    ) = run {
         when (writeType) {
             WriteType.CREATE -> _isDataLoading.value = Event(false)
             WriteType.UPDATE -> {
+                memoId?.let {
+                    _memo = Transformations.switchMap(getMemoUseCase(memoId)) { memo ->
+                        MutableLiveData<Event<MemoModel>>(
+                            Event(
+                                MemoModel(
+                                    id = memo.id,
+                                    title = memo.title,
+                                    body = memo.body,
+                                    thumbnail = memo.thumbnail,
+                                    thumbnailType = memo.thumbnailType,
+                                    imageList = memo.imageList,
+                                    imageLinkList = memo.imageLinkList
+                                )
+                            )
+                        )
+                    }
+                }
             }
         }
         _writeType.value = writeType
     }
 
+    fun setMemoData() {
+        val memo = _memo.value?.peekContent()
+        memo?.let {
+            title.value = it.title
+            body.value = it.body
+            _imageUriList.value = it.imageList ?: mutableListOf()
+            _imageLinkList.value = it.imageLinkList ?: mutableListOf()
+        }
+    }
+
     fun setImageList(imageList: List<Uri>) = run {
-        _imageUriList.value = Event(imageList.map { it.toString() })
+        _imageUriList.value = imageList.map { it.toString() }
+    }
+
+    fun addImageLink(link: String) = run {
+        _imageLinkList.value =
+            _imageLinkList.value?.toMutableList()?.apply { add(link) } ?: mutableListOf(link)
+    }
+
+    fun deleteSelectedImage(position: Int) {
+        val imageUriList =
+            _imageUriList.value?.toMutableList()?.apply { removeAt(position) }
+        imageUriList?.let {
+            _imageUriList.value = it
+        }
+    }
+
+    fun deleteLink(position: Int) {
+        val linkList =
+            _imageLinkList.value?.toMutableList()?.apply { removeAt(position) }
+        linkList?.let {
+            _imageLinkList.value = it
+        }
     }
 
     fun checkData(): Boolean {
@@ -63,20 +119,24 @@ class WriteViewModel(
     }
 
     fun saveMemo() {
-        val imageUriList = _imageUriList.value?.peekContent()?.map { it.toString() }
-        val imageLinkList = _imageLinkList.value?.peekContent()
+        val imageUriList = _imageUriList.value
+        val imageLinkList = _imageLinkList.value
         val thumbnail = getThumbnail(imageUriList, imageLinkList)
         val memo = Memo(
+            id = _memo.value?.peekContent()?.id ?: UUID.randomUUID().toString(),
             title = title.value ?: "",
             body = body.value ?: "",
             thumbnail = thumbnail.first,
             thumbnailType = thumbnail.second,
             imageList = imageUriList,
-            imageLink = imageLinkList
+            imageLinkList = imageLinkList
         )
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                insertMemoUseCase(memo)
+                when (writeType.value) {
+                    WriteType.CREATE -> insertMemoUseCase(memo)
+                    WriteType.UPDATE -> updateMemoUseCase(memo)
+                }
             } catch (e: IOException) {
                 _status.postValue(Event(WriteStatusType.CAN_NOT_SAVE))
             }
