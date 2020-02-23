e2b1abc150f455bf425f418520a7f3c8f2ffba5e
Aiden <sunminlee89@gmail.com>
2020-02-23 20:17:04 +0900

    모델 수정

--- app/src/main/java/com/aiden/memo/presentation/feature/main/MemoModel.kt
+++ app/src/main/java/com/aiden/memo/presentation/feature/main/MemoModel.kt
@@ -1,3 +1,3 @@
 package com.aiden.memo.presentation.feature.main
 
-data class MemoModel(val thumbnailImage: String, val title: String, val description: String)
+data class MemoModel(val thumbnailImage: String?, val title: String, val body: String)
