d5d82f2fd8141b151a367de409a40202569d9735
Aiden <sunminlee89@gmail.com>
2020-02-23 14:33:24 +0900

    ImageView BindingAdapter 추가

--- app/src/main/java/com/aiden/memo/presentation/ext/ImageViewExt.kt
+++ app/src/main/java/com/aiden/memo/presentation/ext/ImageViewExt.kt
@@ -1,5 +1,6 @@
 package com.aiden.memo.presentation.ext
 
+import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.aiden.memo.R
@@ -12,6 +13,17 @@ fun ImageView.bindImageSrc(imageSrc: String?) {
             .load(it)
             .placeholder(R.drawable.image_place_holder)
             .fitCenter()
+            .into(this)
+    }
+}
+
+@BindingAdapter("bind:imageUri")
+fun ImageView.bindImageUri(imageUri: Uri?) {
+    imageUri?.let {
+        Glide.with(this.context)
+            .load(it)
+            .placeholder(R.drawable.image_place_holder)
+            .centerCrop()
             .into(this)
     }
 }
